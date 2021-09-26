bba02010727ccf012f753c21ef0165dfa4dfa2d3
Max Savchuk <avatar2945@gmail.com>
2021-09-26 17:42:18 +0300

    third commit

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const userController = require('./controller/userController');
 const carsController = require('./controller/carsController');
+const validate = require('./middleware/validate');
 const app = express();
 
 app.use(express.json());
@@ -11,7 +12,8 @@ app.get('/', (req, res) => {
 app.post('/', () => {});
 
 app.get('/users', userController.getUsers);
-app.post('/users', userController.createUser); //{}
+
+app.post('/users', validate.validateUser, userController.createUser); //{}
 
 app.get('/users/:userId', userController.getUserById);
 app.patch('/users/0', userController.updateUser); //{}
@@ -21,5 +23,9 @@ app.get('/cars', carsController.getCars);
 app.post('/cars', carsController.createCar);
 app.delete('/cars/:carId', carsController.deleteCar);
 app.patch('/cars/:carId', carsController.updateCar);
+
+app.use((err, req, res, next) => {
+  res.status(500).send(err);
+});
 
 module.exports = app;
