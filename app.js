4975bc0c150e4e0cbe2b3bf5e3da660eccfdf4ef
Max Savchuk <avatar2945@gmail.com>
2021-09-26 15:34:56 +0300

    first commit

---
+++ app.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const userController = require('./controller/userController');
+const carsController = require('./controller/carsController');
+const app = express();
+
+app.use(express.json());
+
+app.get('/', (req, res) => {
+  res.status(200).send('Hello');
+});
+app.post('/', () => {});
+
+app.get('/users', userController.getUsers);
+app.post('/users', userController.createUser); //{}
+
+app.get('/users/:userId', userController.getUserById);
+app.patch('/users/0', userController.updateUser); //{}
+app.delete('/users/10', userController.deleteUser);
+
+app.get('/cars', carsController.getCars);
+app.post('/cars', carsController.createCar);
+app.delete('/cars/:carId', carsController.deleteCar);
+
+module.exports = app;
---
+++ controller/carsController.js
@@ -0,0 +1,39 @@
+const { v4: uuidv4 } = require('uuid');
+
+const carsDb = [
+  {
+    id: uuidv4(),
+    brand: 'BMW',
+    model: 'X6',
+    manufacturedAt: '2020',
+  },
+  {
+    id: uuidv4(),
+    brand: 'Mercedes',
+    model: 'C200',
+    manufacturedAt: '2019',
+  },
+];
+
+module.exports.getCars = (req, res) => {
+  res.status(200).send(carsDb);
+};
+
+module.exports.createCar = (req, res) => {
+  const { body } = req;
+  const newCar = { id: uuidv4(), ...body };
+  carsDb.push(newCar);
+  res.status(201).send(newCar);
+};
+module.exports.deleteCar = (req, res) => {
+  const {
+    params: { carId },
+  } = req;
+  const indexOfCar = carsDb.findIndex(car => carId === car.id);
+  if (indexOfCar > 0 && indexOfCar < carsDb.length) {
+    carsDb.splice(indexOfCar, 1);
+    res.status(200).send(carsDb);
+  } else {
+    res.status(404).send('Not found');
+  }
+};
